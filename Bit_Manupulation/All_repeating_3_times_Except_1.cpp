d00a623d28b42763995407106aaf02cde74c6af9
Prince <alokprince8699@gmail.com>
2022-03-04 09:56:02 +0530

    m

--- Bit_Manupulation/All_repeating_3_times_Except_1.cpp
+++ Bit_Manupulation/All_repeating_3_times_Except_1.cpp
@@ -1,27 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void sol(vector<int> &arr,int n){
-    int n3 = 0;
-    int n31 = 0;
-    int n32 = 0;
-    for(int i = 0 ; i < n; i++){
-        n3 = (1 << i);
-    }
-    int cn3 = 0;
-    int cn31 = 0;
-    int cn32 = 0;
-    for(auto a: arr){
-        cn3 = (a & cn3);
-        cn31 = (a & cn31);
-        cn32 = (a & cn32);
 
-        n31 = (n31 | cn3);
-        n32 = (n32 | cn31);
-        n3 = (n3 | cn32);
-
-    }
-    cout<<n31<<endl;
-}
 int main(){
     int n;
     cin>>n;
@@ -45,16 +24,16 @@ int main(){
         int cn31 = (a & cn31);
         int cn32 = (a & cn32);
 
-        n3 = (n3 & (~cn3));
-        n31 = (n31 | cn3);
+       
         
-        n31 = (n31 & (~cn31));
-        n32 = (n32 | cn31);
+        int tn31 = (n31 & (~cn31));
+        tn32 = (tn32 | cn31);
         
-        n32 = (n32 & (~cn32));
-        n3 = (n3 | cn32);
+        int tn32 = (n32 & (~cn32));
+        tn3 = (tn3 | cn32);
         
-
+        int tn3 = (n3 & (~cn3));
+        tn31 = (tn31 | cn3);
     }
     cout<<n31;
 }
